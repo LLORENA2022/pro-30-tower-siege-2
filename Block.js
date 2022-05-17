30c8a2541226992eb7e0696953160ed5f334f8a6
LLORENA2022 <97839454+LLORENA2022@users.noreply.github.com>
2022-05-17 17:06:43 -0300

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,35 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+          
+          }
+      this.Visiblity = 255;// diminuir a visibilidade
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.image= loadImage("block.png")
+      World.add(world, this.body);
+    }
+    display(){
+      var pos= this.body.position;
+      rectMode(CENTER);
+      rect(pos.x,pos.y,this.width, this.height);
+      console.log(this.body.speed);
+     if(this.body.speed < 3){// velocidade meor que 3 remova o obj
+     imageMode(CENTER);
+     image(this.image,pos.x,pos.y,this.width,this.height);
+    }
+    /*else{
+      World.remove(world, this.body);// matriz de remoção 
+      push();
+      this.Visiblity = this.Visiblity - 5; //diminuir a visibilidade
+      tint(255,this.Visiblity);
+      image(this.image, this.body.position.x, 
+      this.body.position.y,this.width,this.height);
+      pop();
+    }*/
+    else{ World.remove(world, this.body); push(); this.visiblity = this.visiblity - 5; tint(255,this.visiblity); image(this.image, this.body.position.x, this.body.position.y,this.width, this.height); pop(); }
+}
+    }
